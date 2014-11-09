4b0e244519513697cf1b1a8e3ddbf758c0b9f720
Ido Cohen <nrgstar@Ido-Cohens-Mac-mini.local>
2014-11-09 13:39:07 +0000

    Initial Commit

---
+++ Alpha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alpha
+//
+//  Created by Ido Cohen on 09/11/2014.
+//  Copyright (c) 2014 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
